870cfc2370b49f2e41b20b236c5dee311fd941ff
rnoguer22 <rnogugon@myuax.com>
2022-03-20 11:40:39 +0100

    Aplicamos herencia para cada clase correspondiente

--- Clases/Herencia_multiple_caso_real.py
+++ Clases/Herencia_multiple_caso_real.py
@@ -10,13 +10,15 @@ class Ventana:
 class Pared:
     #Constructor
     def __init__(self, orientacion):
+        Ventana.__init__(self)
         self.orientacion = orientacion
 
 
 class Casa:
     #Constructor
     def __init__(self, paredes):
+        Pared.__init__(self)
         self.paredes = paredes
     
     def superficie_acristalada(self):
-        
+        return sum(self.paredes.superficie)
