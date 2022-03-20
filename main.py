0a044ca5dfc280d27521b2805710bfe1337d3e81
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-20 17:17:19 +0100

    ejercicio 1 parte 1 ejecutado en el main

---
+++ main.py
@@ -0,0 +1,8 @@
+from Clases.ejercicio1 import *
+
+if __name__ == "__main__":
+    
+    #EJERCICIO 1
+    punto1 = Punto2D(3,2)
+    punto1.traslacion(1,1)
+    print("El nuevo valor de punto1 es ({},{})".format(punto1.x,punto1.y))
