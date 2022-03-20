a1068f8c946f4e5039611289f453eba3c7bb4292
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-20 17:24:48 +0100

    class Punto3D

--- Clases/ejercicio1.py
+++ Clases/ejercicio1.py
@@ -7,3 +7,12 @@ class Punto2D:
         self.x = self.x + u
         self.y = self.y + v
         return self.x, self.y
+
+class Punto3D(Punto2D):
+    def __init__(self, x, y, z):
+        super().__init__(x,y)
+        self.z = z
+    def traslacion2(self ,a,b,c): 
+        self.x = self.x + a
+        self.y = self.y + b
+        self.z = self.z + c
