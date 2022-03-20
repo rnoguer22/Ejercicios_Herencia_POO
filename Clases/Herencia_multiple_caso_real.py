c8307f18ef5bb764593d4f2806ecdd5660ffdf3c
rnoguer22 <rnogugon@myuax.com>
2022-03-20 12:10:50 +0100

    Herencia multiple mediante el metodo super

--- Clases/Herencia_multiple_caso_real.py
+++ Clases/Herencia_multiple_caso_real.py
@@ -1,24 +1,38 @@
-class Ventana:
+class Pared:
+    #Constructor
+    def __init__(self, orientacion):
+        self.orientacion = orientacion
+        print (self.orientacion)
+
+class Ventana(Pared):
     #Constructor
     def __init__(self, ventana, superficie):
+        super.__init__()
         self.ventana = ventana
         self.superficie = superficie
     
     def get_area(self):
         return self.superficie
 
-class Pared:
-    #Constructor
-    def __init__(self, orientacion):
-        Ventana.__init__(self)
-        self.orientacion = orientacion
-
-
-class Casa:
+class Casa(Ventana):
     #Constructor
     def __init__(self, paredes):
-        Pared.__init__(self)
+        super.__init__()
         self.paredes = paredes
     
     def superficie_acristalada(self):
         return sum(self.paredes.superficie)
+
+# Instanciación de las paredes 
+pared_norte = Pared("NORTE") 
+pared_oeste = Pared("OESTE") 
+pared_sur = Pared("SUR") 
+pared_este = Pared("ESTE") 
+# Instanciación de las ventanas 
+ventana_norte = Ventana(pared_norte, 0.5) 
+ventana_oeste = Ventana(pared_oeste, 1) 
+ventana_sur = Ventana(pared_sur, 2) 
+ventana_este = Ventana(pared_este, 1) 
+# Instanciación de la casa con las 4 paredes 
+casa = Casa([pared_norte, pared_oeste, pared_sur, pared_este]) 
+print(casa.superficie_acristalada()) 
