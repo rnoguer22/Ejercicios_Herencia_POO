f900a49099d7ef77ffa97dc04dd09193d21195f0
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-20 20:04:09 +0100

    meto las herencias dentro del contructor

--- Clases/ejercicio3.py
+++ Clases/ejercicio3.py
@@ -2,13 +2,16 @@ class A:
     def __init__(self,a):
         self.a = a
 class B(A):
-    def __init__(self, b):
+    def __init__(self, b, a):
         self.b = b
+        A.__init__(self, a)
         
 class C(A):
-    def __init__(self, c):
+    def __init__(self, c, a):
         self.c = c
-
+        A.__init__(self, a)
 class D(B, C):
     def __init__(self,d, a, b, c):
         self.d = d
+        B.__init__(self, a, b)
+        C.__init__(self, a, c)
