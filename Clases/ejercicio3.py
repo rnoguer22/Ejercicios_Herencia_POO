53a4ae98d2f29b2e30033268208fb97a28a5cc21
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-20 19:21:48 +0100

    clases de herencia multiple

--- Clases/ejercicio3.py
+++ Clases/ejercicio3.py
@@ -1,3 +1,14 @@
 class A:
     def __init__(self,a):
         self.a = a
+class B(A):
+    def __init__(self, b):
+        self.b = b
+        
+class C(A):
+    def __init__(self, c):
+        self.c = c
+
+class D(B, C):
+    def __init__(self,d, a, b, c):
+        self.d = d
