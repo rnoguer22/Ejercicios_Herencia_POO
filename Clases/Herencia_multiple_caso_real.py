b6b9b2625ad07da04bf5edc698c66eb7e1404b7f
rnoguer22 <rnogugon@myuax.com>
2022-03-20 11:35:09 +0100

    Creacion de la clase Ventana

--- Clases/Herencia_multiple_caso_real.py
+++ Clases/Herencia_multiple_caso_real.py
@@ -3,3 +3,11 @@ class Pared:
     def __init__(self, orientacion):
         self.orientacion = orientacion
     
+class Ventana:
+    #Constructor
+    def __init__(self, ventana, superficie):
+        self.ventana = ventana
+        self.superficie = superficie
+    
+    def get_area(self):
+        return self.superficie
