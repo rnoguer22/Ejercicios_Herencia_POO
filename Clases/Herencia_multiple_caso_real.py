9168f1f5da7e9bc94da7c896b5c0a72cd28bf41c
rnoguer22 <rnogugon@myuax.com>
2022-03-21 16:11:40 +0100

    Introduccion de los ultimos ejercicios en el main.py

--- Clases/Herencia_multiple_caso_real.py
+++ Clases/Herencia_multiple_caso_real.py
@@ -20,17 +20,3 @@ class Casa(Ventana):
     
     def superficie_acristalada(self):
         return sum(self.paredes.superficie)
-
-# Instanciación de las paredes 
-pared_norte = Pared("NORTE") 
-pared_oeste = Pared("OESTE") 
-pared_sur = Pared("SUR") 
-pared_este = Pared("ESTE") 
-# Instanciación de las ventanas 
-ventana_norte = Ventana(pared_norte, 0.5) 
-ventana_oeste = Ventana(pared_oeste, 1) 
-ventana_sur = Ventana(pared_sur, 2) 
-ventana_este = Ventana(pared_este, 1) 
-# Instanciación de la casa con las 4 paredes 
-casa = Casa([pared_norte, pared_oeste, pared_sur, pared_este]) 
-print(casa.superficie_acristalada()) 
